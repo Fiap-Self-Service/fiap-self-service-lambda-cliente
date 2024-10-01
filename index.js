4f3782ef2086877a17c7854fbf582388d99a2d99
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-30 23:05:51 -0300

    feat: ajustar json retorno

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const axios = require('axios');
 
 module.exports.handler = async (event, context) => {
     try {
-        return await axios.get('http://aadfa33dba232459c953a70b3381a909-617e9a439eaa8419.elb.us-east-1.amazonaws.com:3000/clientes/' + event.pathParameters.cpf);
+        const response = await axios.get('http://aadfa33dba232459c953a70b3381a909-617e9a439eaa8419.elb.us-east-1.amazonaws.com:3000/clientes/' + event.pathParameters.cpf);
+
+        // Se a requisição for bem-sucedida, retornamos a resposta com status 200
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                status: 200,
+                data: response.data,  // Retornando os dados recebidos da API pública
+                message: "Sucesso",
+            }),
+        };
     } catch (error) {
         // Em caso de erro, retornamos um status 500 e a mensagem de erro
         return {
