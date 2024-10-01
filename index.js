173651942deba951e315803e6948d1275d6a1f4c
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-30 22:50:33 -0300

    feat: buscar cliente pelo cpf

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+const axios = require('axios');
+
 module.exports.handler = async (event, context) => {
-    // Simulação do objeto mockado
-    const mockResponse = {
-      id: 1,
-      nome: 'Merhy Daychoum',
-      email: 'merhy@gmail.com',
-      cpf: '70234146060'
-    };
-  
-    // Retornando a resposta da Lambda no formato esperado
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        status: 200,
-        data: mockResponse,
-        message: "Sucesso",
-      }),
-    };
-  };
+    try {
+        return await axios.get('http://aadfa33dba232459c953a70b3381a909-617e9a439eaa8419.elb.us-east-1.amazonaws.com:3000/clientes/' + event.pathParameters.cpf);
+    } catch (error) {
+        // Em caso de erro, retornamos um status 500 e a mensagem de erro
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                status: 500,
+                message: "Erro ao processar a requisição",
+                error: error.message,  // Detalhe do erro para debug
+            }),
+        };
+    }
+};
